46e496d2c52aadc6a253ea274e5e078ffcc8f604
Jaswanthiregalla <jaswanthiregalla2655@gmail.com>
2025-06-10 11:54:45 -0700

    A

--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const SERVER_URL =
-  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:5000/api";
+  process.env.REACT_APP_SERVER_URL || "http://localhost:5000/api";
 
 const registerUser = (data) => {
   return axios.post(SERVER_URL + "/register", data);
