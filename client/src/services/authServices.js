911a1ad367528bb28be9fb9d5fbe2b8e207c78ef
Jaswanthiregalla <jaswanthiregalla2655@gmail.com>
2025-06-10 09:31:26 -0700

    Add

--- client/src/services/authServices.js
+++ client/src/services/authServices.js
@@ -1,20 +1,18 @@
-import axios from 'axios'
-const SERVER_URL = 'http://localhost:5000/api'
-
+import axios from "axios";
+const SERVER_URL =
+  process.env.NODE_ENV === "production" ? "/api" : "http://localhost:5000/api";
 
 const registerUser = (data) => {
-    return axios.post(SERVER_URL+'/register',data)
-}
+  return axios.post(SERVER_URL + "/register", data);
+};
 
 const loginUser = (data) => {
-    return axios.post(SERVER_URL+'/login',data)
-
-}
+  return axios.post(SERVER_URL + "/login", data);
+};
 
 const AuthServices = {
-    registerUser,
-    loginUser
-}
+  registerUser,
+  loginUser,
+};
 
-
-export default AuthServices  
+export default AuthServices;
