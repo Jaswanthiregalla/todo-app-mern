248a86c62640db08f4b72ce3a830ce4b1f40acf4
Jaswanthiregalla <jaswanthiregalla2655@gmail.com>
2025-06-10 22:30:40 -0700

    Template

--- client/src/services/toDoServices.js
+++ client/src/services/toDoServices.js
@@ -3,26 +3,27 @@ import { getUserDetails } from "../util/GetUser";
 
 const SERVER_URL =
   process.env.REACT_APP_SERVER_URL || "http://localhost:5000/api";
+
 const authHeaders = () => {
   let userToken = getUserDetails()?.token;
   return { headers: { Authorization: userToken } };
 };
 
 const createToDo = (data) => {
-  return axios.post(SERVER_URL + "todo/create-to-do", data, authHeaders());
+  return axios.post(`${SERVER_URL}/todo/create-to-do`, data, authHeaders());
 };
 
 const getToDo = (userId) => {
-  return axios.get(SERVER_URL + "todo/get-all-to-do/" + userId, authHeaders());
+  return axios.get(`${SERVER_URL}/todo/get-all-to-do/${userId}`, authHeaders());
 };
 
 const deleteToDo = (id) => {
-  return axios.delete(SERVER_URL + "todo/delete-to-do/" + id, authHeaders());
+  return axios.delete(`${SERVER_URL}/todo/delete-to-do/${id}`, authHeaders());
 };
 
 const updateToDo = (id, data) => {
   return axios.patch(
-    SERVER_URL + "todo/update-to-do/" + id,
+    `${SERVER_URL}/todo/update-to-do/${id}`,
     data,
     authHeaders()
   );
